85d2837c1050a8e6e6f9aab3d9ef32ee83a6e1d0
liran bratt <95234790+LiranBratt2121@users.noreply.github.com>
2022-09-26 12:44:27 +0300

    Add files via upload

---
+++ hebrewCommands.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#define משתנה_מספרי int
+#define משתנה_סמלי char
+#define משתנה_בוליאני bool
+#define  סהוצא std::cout
+#define  סקבל std::cin
+#define סיים_שורה std::endl
+#define פור for 
+#define במקרה_ש if
+#define אחרת_במקרה_ש else if
+#define אחרת else
+#define חלל void
+#define החזר return
+#define ציבורי public
+#define לך_ל goto
+#define בזמן_ש while
+#define סטטי static
+#define אמת true
+#define שקר false
+#define מערכת system
